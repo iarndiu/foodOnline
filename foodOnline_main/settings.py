f690685a2196fd0eaa26387f6ca5713d8712608e
iarndiu Z <celeste2106@163.com>
2023-07-29 19:44:20 -0700

    forgot password feature

--- foodOnline_main/settings.py
+++ foodOnline_main/settings.py
@@ -136,3 +136,11 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
+
+# email configuration
+EMAIL_HOST = config('EMAIL_HOST')
+EMAIL_PORT = config('EMAIL_PORT', cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS')
+DEFAULT_FROM_EMAIL = config('DEFAULT_FROM_EMAIL')
