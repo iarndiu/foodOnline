588c9b6e871b45dfebf99ae3944987257d1addbe
iarndiu Z <celeste2106@163.com>
2023-08-05 17:14:07 -0700

    food CRUD

--- menu/forms.py
+++ menu/forms.py
@@ -11,3 +11,11 @@ class CategoryForm(forms.ModelForm):
     class Meta:
         model = Category
         fields = ['category_name', 'description']
+
+
+class FoodItemForm(forms.ModelForm):
+    image = forms.FileField(widget=forms.FileInput(attrs={'class': 'btn btn-info w-100'}), validators=[allow_only_images_validator])
+
+    class Meta:
+        model = FoodItem
+        fields = ['category', 'food_title', 'description', 'price', 'image', 'is_available']
