92311d230584279a8d3a745505e955817167177e
iarndiu Z <celeste2106@163.com>
2023-09-04 17:39:15 -0700

    uncomment post save signal

--- accounts/signals.py
+++ accounts/signals.py
@@ -3,19 +3,19 @@ from django.dispatch import receiver
 from .models import User, UserProfile
 
 
-# @receiver(post_save, sender=User)
-# def create_profile_receiver(sender, instance, created, **kwargs):
-#     if created:
-#         UserProfile.objects.create(user=instance)
-#         # print('user profile is created')
-#     else:
-#         try:
-#             profile = UserProfile.objects.get(user=instance)
-#             profile.save()
-#         except:  # update user but user profile does not exist
-#             UserProfile.objects.create(user=instance)
-#         #     print('user profile does not exist, but is created')
-#         # print('user is updated')
+@receiver(post_save, sender=User)
+def create_profile_receiver(sender, instance, created, **kwargs):
+    if created:
+        UserProfile.objects.create(user=instance)
+        # print('user profile is created')
+    else:
+        try:
+            profile = UserProfile.objects.get(user=instance)
+            profile.save()
+        except:  # update user but user profile does not exist
+            UserProfile.objects.create(user=instance)
+        #     print('user profile does not exist, but is created')
+        # print('user is updated')
 
 
 @receiver(pre_save, sender=User)
